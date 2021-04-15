3a089fbaf788fede15cf42002d555c665f36b46b
KetolaC <78930559+KetolaC@users.noreply.github.com>
2021-04-15 16:46:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from VideoClasses import *
+from DroneWASD import *
+from HalfLife3 import *
+from Snake import SnakeGame
 
-game = DroneInvaders(-200, 200, 0, 400)
+arcade = [DroneInvaders(-200, 200, 0, 400), None, playHL3()]
+
+gameChoice = input("Type a number to select a game:\n1. Drone Invaders\n2. Duck Hunt\n3. Half-Life 3\n")
+
+gameSelect = int(gameChoice)
+
+game = arcade[gameSelect - 1]
+
 game.play()
+
